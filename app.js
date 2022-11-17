bc4b51ce70386971ac467a4634ffaf94be05809a
Daniel Miolan <miolan96@gmail.com>
2022-11-16 19:33:59 -0500

    Create express app to run server.

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("init")
+const express = require('express')
+const app = express()
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('App running!')
+})
+
+app.listen(port)
+
+console.log(`Server running at http://${hostname}:${port}/`);
